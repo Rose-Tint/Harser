7714486e6631966ff8bc2681a8f4cf75966d70b3
Rose <71909018+Xenovia02@users.noreply.github.com>
2022-01-12 18:27:00 +0000

    added user state functionality to Harser.Parser

--- main.hs
+++ main.hs
@@ -1,12 +1,7 @@
 module Main where
 
-import System.IO (hSetEcho, stdin)
-
-import Examples.CalcInterpreter (mainLoop)
-import Examples.SimpleGrammar ()
+import Harser.Parser ()
 
 
 main :: IO ()
-main = do
-    hSetEcho stdin False
-    mainLoop
+main = putStrLn "... Done!"
