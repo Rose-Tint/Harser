4d89b734b67acc185f6d13fdf4ff6e985116e84b
Rose <71909018+Xenovia02@users.noreply.github.com>
2022-02-02 04:42:38 +0000

    began documentation for Harser.Combinators

--- Harser/Combinators.hs
+++ Harser/Combinators.hs
@@ -50,41 +50,49 @@ oneOrMore :: Parser s u a -> Parser s u [a]
 oneOrMore = some
 
 
--- | restores stream to previous state on failure
+-- |If the given parser fails, resets the current
+-- state. Does not consume input on failure.
 try :: Parser s u a -> Parser s u a
 try (Parser f) = Parser (\s -> case f s of
     (_, Failure e)  -> (s, Failure e)
     (s', Success x) -> (s', Success x))
 
 
+-- |@'exactly' t@ is equivalent to @'fulfill'
+-- (== t)@
 exactly :: (Eq t, Stream s t) => t -> Parser s u t
 exactly t = fulfill (== t)
 
 
--- | takes a parser and an default value. upon failure,
--- | succeeds with the default value.
+-- |Takes a parser and a default value. Upon failure,
+-- succeeds with the default value.
 option :: Parser s u a -> a -> Parser s u a
 option (Parser a) d = Parser (\s -> case a s of
     (s', Failure _) -> (s', Success d)
     (s', Success x) -> (s', Success x))
 
 
--- | 1+
+-- |@delim `'splits'` psr@ parses one or more
+-- @psr@'s, seperated by @delim@. Typically used
+-- in infix for readability purposes.
 splits :: Parser s u a -> Parser s u b -> Parser s u [b]
 splits s p = (:) <$> p <*> zeroOrMore (s *> p)
 
 
--- | 0+
+-- |Same as 'splits', but parses zero or more.
 splits' :: Parser s u a -> Parser s u b -> Parser s u [b]
 splits' s p = splits s p <?> pure []
 
 
+-- |@'atLeast' n p@ parses @n@ or more instances of
+-- @p@
 atLeast :: Int -> Parser s u a -> Parser s u [a]
 atLeast 0 p = zeroOrMore p
 atLeast 1 p = oneOrMore p
 atLeast n p = (++) <$> (count n p) <*> zeroOrMore p
 
 
+-- |@'atMost' n p@ parses at most @n@ instances of @p@
 atMost :: Int -> Parser s u a -> Parser s u [a]
 atMost 0 _ = pure []
 atMost 1 p = fmap (:[]) p
@@ -95,27 +103,34 @@ atMost n p@(Parser a) = Parser (\s -> case a s of
         (s'', Success xs) -> (s'', Success (x:xs)))
 
 
+-- |@'count' n p@ parses exactly @n@ instances of @p@
 count :: Int -> Parser s u a -> Parser s u [a]
 count 0 _ = return []
 count n p = (:) <$> p <*> (count (n - 1) p)
 
 
+-- |@'skip' p@ parses @p@ (consuming input), and
+-- returns @()@
 skip :: Parser s u a -> Parser s u ()
 skip (Parser a) = Parser (\s -> case a s of
     (s', Failure e) -> (s', Failure e)
     (s', Success _) -> (s', pure ()))
 
 
--- | 0+
+-- |@'skips' p@ runs p zero or more times, then
+-- returns @()@
 skips :: Parser s u a -> Parser s u ()
 skips p = zeroOrMore p >> pure ()
 
 
--- | 1+
+-- |@'skips' p@ runs p one or more times, then
+-- returns @()@
 skips' :: Parser s u a -> Parser s u ()
 skips' p = oneOrMore p >> pure ()
 
 
+-- |@'skipn' n p@ skips exactly @n@ instances of
+-- @p@
 skipn :: Int -> Parser s u a -> Parser s u ()
 skipn n p = count n p >> pure ()
 
